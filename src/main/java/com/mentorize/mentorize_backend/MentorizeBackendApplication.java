1dadbd4d48e2aa09e4f904f9c73314274595b08b
Endrick <ehpb2014@gmail.com>
2025-03-14 22:14:10 -0300

    first commit

---
+++ src/main/java/com/mentorize/mentorize_backend/MentorizeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mentorize.mentorize_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorizeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorizeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentorize/mentorize_backend/MentorizeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentorize.mentorize_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorizeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
